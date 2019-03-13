1d89b9eac350ed27635d98275748054ff83fba3f
jordanhudgens <jordan@devcamp.com>
2019-03-13 01:07:34 -0500

    Added initial create action

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -14,10 +14,27 @@ class AccountsController < ApplicationController
   end
 
   def create
-    # TODO
+    if @current_user
+      account = Account.new(account_params)
+      account.user_id = @current_user.id
+
+      if account.save
+        render json: account, status: :created
+      else
+        render json: account.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { status: 401 }
+    end
   end
 
   def destroy
     # TODO
   end
+
+  private
+
+    def account_params
+      params.require(:account).permit(:username)
+    end
 end
