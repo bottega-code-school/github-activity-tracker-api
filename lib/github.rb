aea098ab4397d2850c24218919c32824bbf1d59d
jordanhudgens <jordan@devcamp.com>
2019-03-13 01:46:10 -0500

    Added secured api credentials to the github library query

--- lib/github.rb
+++ lib/github.rb
@@ -1,5 +1,9 @@
 module Github
+  def self.api_credentials
+    "client_id=#{Rails.application.credentials.github[:client_id]}&client_secret=#{Rails.application.credentials.github[:client_secret]}"
+  end
+
   def self.username_search username
-    HTTParty.get("https://api.github.com/users/#{username}")
+    HTTParty.get("https://api.github.com/users/#{username}?#{self.api_credentials}")
   end
 end
