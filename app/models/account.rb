bc7831a50b15b1389a1ec7d7f7ee65b3e7dc2d45
jordanhudgens <jordan@devcamp.com>
2019-03-14 00:04:07 -0500

    Added debugging

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,6 +8,8 @@ class Account < ApplicationRecord
   private
 
     def unique_username_for_user
+      puts "unique_username_for_user" * 500, self.username, "unique_username_for_user" * 500
+      puts "collection" * 500, self.user.accounts.pluck(:username), "collection" * 500
       if self.user.accounts.pluck(:username).include? self.username
         false
       else
