4f0b3f35407b75203117c4095e5971ae2016cd5c
jordanhudgens <jordan@devcamp.com>
2019-03-13 23:29:49 -0500

    Updated session initializer with domain rule that allows for localhost cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: '_bottega_activity_session', domain: 'bottega-activity-tracker-api.herokuapp.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_bottega_activity_session', domain: 'bottega-activity-tracker-api.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_bottega_activity_session'
+end
 
