55cca0c861326cc3714491438b036e1495af9afb
jordanhudgens <jordan@devcamp.com>
2019-03-15 00:29:05 -0500

    Updated accounts serializer

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,6 +1,14 @@
 class AccountSerializer < ActiveModel::Serializer
   attributes :id,
              :login
+             :user_id,
+             :avatar_url,
+             :bio,
+             :public_repos,
+             :public_gists,
+             :followers,
+             :following,
+             :member_since
 
   belongs_to :user
 end
