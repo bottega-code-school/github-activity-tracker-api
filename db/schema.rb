0c43d95a9a6f41c6b2edb46372cac4e74ad5288e
jordanhudgens <jordan@devcamp.com>
2019-03-16 21:54:29 -0500

    Added github id to keep track of the event id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_042518) do
+ActiveRecord::Schema.define(version: 2019_03_17_025335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_03_16_042518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "commit_count"
+    t.text "github_event_id"
     t.index ["account_id"], name: "index_events_on_account_id"
   end
 
