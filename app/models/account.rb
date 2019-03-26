a3ff7513e7a52d95135bbe5592a442ca36a9358a
jordanhudgens <jordan@devcamp.com>
2019-03-26 17:41:15 -0400

    Updated validation for states

--- app/models/account.rb
+++ app/models/account.rb
@@ -36,7 +36,7 @@ class Account < ApplicationRecord
 
     def state_abbr_exists
       state_abbr_list = %w{AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY}
-      if state_abbr_list.include?(self.state)
+      unless state_abbr_list.include?(self.state)
         errors.add(:state, "State does not exist")
       end
     end
