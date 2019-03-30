9ec3dd849bd18e505506258164428ffe93ab8d19
jordanhudgens <jordan@devcamp.com>
2019-03-30 11:48:38 -0400

    Added debugging and conditional for nested query

--- app/controllers/grouped_events_controller.rb
+++ app/controllers/grouped_events_controller.rb
@@ -7,8 +7,12 @@ class GroupedEventsController < ApplicationController
         account_ids = @current_user.accounts.pluck(:id)
         if Event.where(id: account_ids).any?
           events = Event.where(account_id: account_ids).group_by(&:account_id)
-          events_grouped_by_date = account_ids.map do |account_id| 
-            events[account_id].group_by(&:date)
+          events_grouped_by_date = account_ids.map do |account_id|
+            puts "events[account_id]" * 500, events[account_id], "group by " * 500
+            puts "EVENTS" * 500, events.inspect, "events" * 500
+            if events[account_id]
+              events[account_id].group_by(&:date)
+            end
           end
           render json: { events: events_grouped_by_date }
         else
