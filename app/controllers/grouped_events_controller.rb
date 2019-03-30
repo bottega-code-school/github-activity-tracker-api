4396210d2a0622e590572f1a939c3b23ef0b929b
jordanhudgens <jordan@devcamp.com>
2019-03-30 13:42:30 -0400

    Rebuilt the query to include a date range and structure that matches what is needed by the d3 dot chart

--- app/controllers/grouped_events_controller.rb
+++ app/controllers/grouped_events_controller.rb
@@ -4,21 +4,23 @@ class GroupedEventsController < ApplicationController
   def index
     if @current_user
       if @current_user.accounts.any?
-        account_ids = @current_user.accounts.pluck(:id)
+        accounts = @current_user.accounts.pluck(:id, :login)
+
         if Event.where(id: account_ids).any?
           events = Event.where(account_id: account_ids).group_by(&:account_id)
+          date_range.each_with_object([]).with_index do |(date, arr), idx|
+            hash = Hash.new
+            hash["bin"] = idx
 
-          account_hash = account_ids.each_with_object({}) do |account_id, hash|
-            hash[account_id] = []
-          end
-
-          account_ids.each do |account_id|
-            if events[account_id]
-              account_hash[account_id] = events[account_id].group_by(&:date)
+            hash["bins"] = accounts.each_with_object([]) do |account, nested_arr|
+              nested_hash = Hash.new
+              nested_hash["bin"] = acount[0]
+              nested_hash["login"] = account[1]
+              count = Event.where(id: account[0], date: date).length
             end
-          end
 
-          render json: { events: account_hash }
+            arr << hash
+          end
         else
           render json: { events: [] }
         end
@@ -29,4 +31,10 @@ class GroupedEventsController < ApplicationController
       render json: { status: 401 }
     end
   end
+
+  private
+
+    def date_range(a_range = 2.months.ago..0.days.ago)
+      (a_range.begin.to_date..a_range.end.to_date).to_a
+    end
 end
