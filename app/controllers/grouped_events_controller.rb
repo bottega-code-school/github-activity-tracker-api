cd3ecd81d01a6ffe8524a3e0d0c63214e94868ab
jordanhudgens <jordan@devcamp.com>
2019-03-30 11:43:11 -0400

    Built out nested group by query that groups events by account and then by date

--- app/controllers/grouped_events_controller.rb
+++ app/controllers/grouped_events_controller.rb
@@ -6,8 +6,11 @@ class GroupedEventsController < ApplicationController
       if @current_user.accounts.any?
         account_ids = @current_user.accounts.pluck(:id)
         if Event.where(id: account_ids).any?
-          events = Event.where(account_id: account_ids).group_by(&:date)
-          render json: { events: events }
+          events = Event.where(account_id: account_ids).group_by(&:account_id)
+          events_grouped_by_date = account_ids.map do |account_id| 
+            events[account_id].group_by(&:date)
+          end
+          render json: { events: events_grouped_by_date }
         else
           render json: { events: [] }
         end
