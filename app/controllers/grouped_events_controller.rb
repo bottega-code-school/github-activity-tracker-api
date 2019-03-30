9422f80ee1908d9b78ac39d8ec7fb2cd48776785
jordanhudgens <jordan@devcamp.com>
2019-03-30 12:06:27 -0400

    Testing out new query that includes a named hash of accounts

--- app/controllers/grouped_events_controller.rb
+++ app/controllers/grouped_events_controller.rb
@@ -7,14 +7,18 @@ class GroupedEventsController < ApplicationController
         account_ids = @current_user.accounts.pluck(:id)
         if Event.where(id: account_ids).any?
           events = Event.where(account_id: account_ids).group_by(&:account_id)
-          events_grouped_by_date = account_ids.map do |account_id|
-            puts "events[account_id]" * 500, events[account_id], "group by " * 500
-            puts "EVENTS" * 500, events.inspect, "events" * 500
+
+          account_hash = account_ids.each_with_object({}) do |account_id, hash|
+            hash[account_id] = []
+          end
+
+          account_ids.each do |account_id|
             if events[account_id]
-              events[account_id].group_by(&:date)
+              account_hash[account_id] = events[account_id].group_by(&:date)
             end
           end
-          render json: { events: events_grouped_by_date }
+
+          render json: { events: account_hash }
         else
           render json: { events: [] }
         end
