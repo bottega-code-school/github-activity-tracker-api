82fb03f899d54f994b70c4fd76ad37345e8e1f74
jordanhudgens <jordan@devcamp.com>
2019-05-10 14:02:48 -0700

    Built out an endpoint for pulling in the hire rates

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :accounts, only: [:index, :show, :create, :destroy, :update]
   resources :locations, only: [:index]
   resources :grouped_events, only: [:index]
+  get :hire_rates, to: "grouped_events#hire_rates"
 
   resources :password_resets, only: [:create, :update, :show]
   resources :registrations, only: [:create]
